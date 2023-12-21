8fa16c745d44d1fe1953ae651adc204d66fabef5
Evgeny Bliznyuk <it@firelli.ru>
2023-12-22 01:02:48 +0300

    start models

--- orders/backend/models.py
+++ orders/backend/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+class Shop(models.Model):
+    name = models.CharField(max_length=100)
+    url = models.CharField()
+
+    def __str__(self):
+        return f"<class Shop {self.name}: {self.url}>"
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    shops = models.ManyToManyField("Shop")
+
+    def __str__(self):
+        return f"<class Category {self.name}>"
+
